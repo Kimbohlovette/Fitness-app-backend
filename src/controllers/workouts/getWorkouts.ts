7ff056e13595145dcf076de4a3e55a486d3ad851
KimbohLovette <kimbohlovette@gmail.com>
2023-04-05 14:58:24 +0100

    Added MongoDB to app

--- src/controllers/workouts/getWorkouts.ts
+++ src/controllers/workouts/getWorkouts.ts
@@ -1,6 +1,14 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import mongoClient from '../../mongoDB.config';
+import { title } from 'process';
 
 export const getWorkouts = (req: Request, res: Response) => {
 	// @TODO: Write get workouts logic here
-	return res.status(200).json({ success: true, workouts: [] });
+	try {
+		const database = mongoClient.db('fitnessDB');
+		const workouts = database.collection('workouts');
+		console.log(workouts.find({}).toArray());
+		return res.status(200).json({ success: true, workouts: [] });
+	} finally {
+	}
 };
